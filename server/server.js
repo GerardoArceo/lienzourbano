c3bbcfef6fd6266abe938b18ed3a5e7d87fdb02d
= <=>
2019-11-23 12:31:10 -0600

    commit

--- server/server.js
+++ server/server.js
@@ -2,11 +2,8 @@
 require('./config/config');
 const express = require('express');
 const bodyParser = require('body-parser');
-const https = require('https');
-const fs = require('fs');
 
 const app = express();
-const port = process.env.PORT;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ limit: '10mb', extended: true }));
@@ -14,21 +11,20 @@ app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
 app.use(require('./routes/index')); // Configuración global de rutas
 
-if (process.env.NODE_ENV === 'dev') {
-    process.env.HOST_DB = '127.0.0.1';
-    app.listen(port);
-} else {
-    https.createServer({
-        key: fs.readFileSync('/etc/ssl/private/private.key'),
-        cert: fs.readFileSync('/etc/ssl/private/certificate.crt')
-    }, app).listen(3001);
-}
-console.log(`SERVER IS LISTENING: 
-NODE_ENV: ${process.env.NODE_ENV} 
-PORT: ${port} 
-PORT_DB: ${process.env.PORT_DB} 
-HOST_DB: ${process.env.HOST_DB} 
-PASS_DB: ${process.env.PASS_DB}
-SEED: ${process.env.SEED}
-RECAPTCHA_KEY: ${process.env.RECAPTCHA_KEY}
-`);
+// Serve static files....
+app.use(express.static(__dirname + '/dist/lienzourbano'));
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/lienzourbano/index.html'));
+});
+
+// default Heroku PORT
+app.listen(process.env.PORT || 3000);
