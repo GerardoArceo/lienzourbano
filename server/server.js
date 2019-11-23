ef04806a8db01d72b6d78c1fdd2cb525e694b1a7
= <=>
2019-11-23 14:19:12 -0600

    commit

--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
 //REQUIREDS
-require('./config/config');
 const express = require('express');
+const path = require('path');
+
 const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json({ limit: '10mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json({ limit: '10mb', extended: true }));
+// app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
-app.use(require('./routes/index')); // Configuración global de rutas
+// app.use(require('./routes/index'));
 
 // Serve static files....
 app.use(express.static(__dirname + '/dist/lienzourbano'));
