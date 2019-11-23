1184535cb6e55df7a8eee16b1a540ccb22948989
= <=>
2019-11-23 12:48:24 -0600

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDkXsQ1le0FU62Vy40_uIngTunYW7XoGUE',
+    authDomain: 'firechat-defcb.firebaseapp.com',
+    databaseURL: 'https://firechat-defcb.firebaseio.com',
+    projectId: 'firechat-defcb',
+    storageBucket: '',
+    messagingSenderId: '1025495503633',
+    appId: '1:1025495503633:web:4a5ab8d2833e8e9da7f2b0'
+  }
 };
